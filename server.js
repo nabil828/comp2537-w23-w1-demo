b9d5e5460b79379790defa5ffc163f28ab667bef
Nabil M. Al-Rousan <rousani@gmail.com>
2023-04-24 16:02:31 -0700

    middleware example

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const app = express();
 
-
-app.get('/', (req, res) => {
-    res.send('Hello World');
+app.listen(3000, () => {
+  console.log('server is running on port 3000');
 });
 
 
-app.listen(3000, () => {
-    console.log('server is running on port 3000');
+
+
+// only for authenticated users
+app.use((req, res, next) => {
+  console.log('global middleware');
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.send('<h1> Hello World </h1>');
+});
+app.get('/anotherRoute', (req, res, next) => {
+  console.log(1);
+  next()
+}, (req, res) => {
+  console.log(2);
+  res.send('<h1> anotherRoute</h1>');
 });
