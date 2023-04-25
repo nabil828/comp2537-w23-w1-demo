ecdce770c9c70cd45bad6b47101f0992ee3a9f2f
Nabil M. Al-Rousan <rousani@gmail.com>
2023-04-24 17:32:27 -0700

    Mongoose connection, schema, and model. Also, refactoring abit

--- server.js
+++ server.js
@@ -1,89 +1,10 @@
-const express = require('express');
-const app = express();
-const session = require('express-session');
+const mongoose = require('mongoose');
+const app = require('./app');
+main().catch(err => console.log(err));
 
-app.listen(3000, () => {
-  console.log('server is running on port 3000');
-});
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/comp2537w1');
 
-
-const users = [
-  {
-    username: 'admin',
-    password: 'admin',
-    type: 'administrator'
-  },
-  {
-    username: 'user1',
-    password: 'pass1',
-    type: 'non-administrator'
-  }
-]
-
-app.use(session({
-  secret: 'the secret is sky color is blue ' // bad secret
-}));
-
-// public routes
-app.get('/', (req, res) => {
-  res.send('<h1> Hello World </h1>');
-});
-
-
-app.get('/login', (req, res) => {
-  res.send(`
-    <form action="/login" method="post">
-      <input type="text" name="username" placeholder="Enter your username" />
-      <input type="password" name="password" placeholder="Enter your password" />
-      <input type="submit" value="Login" />
-    </form>
-  `)
-
-});
-
-// GLOBAL_AUTHENTICATED = false;
-app.use(express.urlencoded({ extended: false }))
-// built-in middleware function in Express. It parses incoming requests with urlencoded payloads and is based on body-parser.
-
-app.post('/login', (req, res) => {
-  // set a global variable to true if the user is authenticated
-  if (users.find((user) => user.username == req.body.username && user.password == req.body.password)) {
-    req.session.GLOBAL_AUTHENTICATED = true;
-    req.session.loggedUsername = req.body.username;
-    req.session.loggedPassword = req.body.password;
-  }
-  res.redirect('/');
-
-});
-
-
-
-// only for authenticated users
-const authenticatedOnly = (req, res, next) => {
-  if (!req.session.GLOBAL_AUTHENTICATED) {
-    return res.status(401).json({ error: 'not authenticated' });
-  }
-  next(); // allow the next route to run
-};
-app.use(authenticatedOnly);
-
-
-app.get('/protectedRoute', (req, res) => {
-  res.send('<h1> protectedRoute </h1>');
-});
-
-
-// only for admins
-const protectedRouteForAdminsOnlyMiddlewareFunction = (req, res, next) => {
-  // console.log(users.find((user) => user.username == req.session.loggedUsername && user.password == req.session.loggedPassword));
-  // console.log(users.find((user) => user.username == req.session.loggedUsername && user.password == req.session.loggedPassword)?.type);
-  if (users.find((user) => user.username == req.session.loggedUsername && user.password == req.session.loggedPassword)?.type != 'administrator') {
-    return res.send('<h1> You are not an admin </h1>')
-  }
-  next(); // allow the next route to run
-};
-app.use(protectedRouteForAdminsOnlyMiddlewareFunction);
-
-app.get('/protectedRouteForAdminsOnly', (req, res) => {
-  res.send('<h1> protectedRouteForAdminsOnly </h1>');
-});
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+  console.log("connected to db");
+}
