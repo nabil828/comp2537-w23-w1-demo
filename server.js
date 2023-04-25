cc0a06edab3284afbd09eb854ba7acca063e4b86
Nabil M. Al-Rousan <rousani@gmail.com>
2023-04-25 00:04:11 -0700

    hosting the db on Atlas and using dotenv

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
 const app = require('./app');
-main().catch(err => console.log(err));
+const dotenv = require('dotenv');
+dotenv.config();
+// require('dotenv').config()
 
+main().catch(err => console.log(err));
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/comp2537w1');
+  await mongoose.connect(`mongodb+srv://${process.env.ATLAS_DB_USER}:${process.env.ATLAS_DB_PASSWORD}@cluster0.lbm8g.mongodb.net/comp2537w1?retryWrites=true&w=majority`);
+  // await mongoose.connect('mongodb://127.0.0.1:27017/comp2537w1');
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
   console.log("connected to db");
